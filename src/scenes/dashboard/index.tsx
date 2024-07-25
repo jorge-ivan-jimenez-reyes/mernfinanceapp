dc69f013779a4058f4f1d4eafc86d2e1147f6e27
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2024-07-25 15:11:16 -0600

    added state

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,7 +1,7 @@
 import { Box, useMediaQuery } from "@mui/material";
-// import Row1 from "./Row1";
-// import Row2 from "./Row2";
-// import Row3 from "./Row3";
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
 
 const gridTemplateLargeScreens = `
   "a b c"
@@ -70,9 +70,9 @@ const Dashboard = () => {
             }
       }
     >
-      {/* <Row1 />
+      <Row1 />
       <Row2 />
-      <Row3 /> */}
+      <Row3 />
     </Box>
   );
 };
